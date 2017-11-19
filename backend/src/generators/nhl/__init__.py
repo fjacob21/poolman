ab977f0e27956e58a27fb0bc9e27a8644204add9
Frederic jacob <frederic.jacob.78@gmail.com>
2017-11-19 00:26:25 -0500

    Begin work on NHL tree generator

--- backend/src/generators/nhl/__init__.py
+++ backend/src/generators/nhl/__init__.py
@@ -1,5 +1,7 @@
 from .nhlteamgenerator import NHLTeamGenerator
 from .nhlgamegenerator import NHLGameGenerator
 from .nhlstandinggenerator import NHLStandingGenerator
+from .nhlmatchuptreegenerator import NHLMatchupTreeGenerator
 
-__all__ = [NHLTeamGenerator, NHLGameGenerator, NHLStandingGenerator]
+__all__ = [NHLTeamGenerator, NHLGameGenerator, NHLStandingGenerator,
+           NHLMatchupTreeGenerator]
